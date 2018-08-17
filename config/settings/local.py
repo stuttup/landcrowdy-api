b0f60d1b2529a91132c3fadc74a1d47f1fa68337
Cheikhou <cni@tentelemed.com>
2018-08-17 20:15:31 +0200

    add allowed host in local settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -2,4 +2,7 @@ from config.settings.base import *
 from config.settings.base import env
 
 DEBUG = True
+
 SECRET_KEY = env('SECRET_KEY', default='c#-!x^di-(n7@h@_2p@j(%^ce#8-@m=ager%x_zfqq%034qfdb')
+
+ALLOWED_HOSTS = ["localhost","0.0.0.0","127.0.0.1",]
