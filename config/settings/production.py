9ead3de253d97a6679e825ef2491348f6ec080f9
Cheikhou <cni@tentelemed.com>
2018-08-17 19:06:03 +0200

    add allowed host in prod settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,3 +2,5 @@ from config.settings.base import *
 from config.settings.base import env
 
 DEBUG = False
+
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['localhost','127.0.0.1', '.niangular.com'])
