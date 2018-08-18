671bacb3ea0d83ef89ff86fbb7a0bf951644ce16
cheikhou <niangcheikhou@gmail.com>
2018-08-18 09:03:09 +0200

    add docs view

--- config/urls.py
+++ config/urls.py
@@ -6,12 +6,15 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 
 schema_view = get_schema_view(title='Landcrowdy API')
+docs_views = include_docs_urls(title='Landcrowdy API')
 
 urlpatterns = [
     path('api/v1/schema/', schema_view),
+    path('api/v1/docs/', docs_views),
     path('api/v1/', include('landcrowdy.ads.urls')),
     path('api/v1/', include('landcrowdy.users.urls')),
     path('api/v1/admin/', admin.site.urls),
