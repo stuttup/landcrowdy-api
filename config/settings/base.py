0b65ea9448b199332e0fb91158f0cc1a9a2274a7
Cheikhou Niang <cheikhou@vps575846.ovh.net>
2018-08-18 07:52:40 +0200

    minor prod setup fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = str(BASE_DIR('staticfiles'))
+STATIC_ROOT = str(BASE_DIR('static'))
 
 # Email
 EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
--- config/settings/production.py
+++ config/settings/production.py
@@ -3,8 +3,21 @@ from config.settings.base import env
 
 DEBUG = False
 
+SECRET_KEY = env('SECRET_KEY', default='c#-!x^di-(n7@h@_2p@j(%^ce#8-@m=ager%x_zfqq%034qfdb')
+
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['localhost','127.0.0.1', '.niangular.com'])
 # Database
-DATABASES['default'] = env.db('DATABASE_URL')
-DATABASES['default']['ATOMIC_REQUESTS'] = True
-DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)
+#DATABASES = {}
+#DATABASES['default'] = env.db('DATABASE_URL')
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+	'ATOMIC_REQUESTS': True,
+	'CONN_MAX_AGE': env.int('CONN_MAX_AGE', default=60)
+    }
+}
