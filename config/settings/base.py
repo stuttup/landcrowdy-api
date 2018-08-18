b52e9e177fe2bb4105b8bcdf639da34743f148d3
cheikhou <niangcheikhou@gmail.com>
2018-08-18 03:56:55 +0200

    switch prod database to postgres

--- config/settings/base.py
+++ config/settings/base.py
@@ -112,7 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = str(BASE_DIR('static'))
+STATIC_ROOT = str(BASE_DIR('staticfiles'))
+
+# Email
+EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
 
 # DRF setup
 # http://www.django-rest-framework.org/
--- config/settings/production.py
+++ config/settings/production.py
@@ -4,3 +4,7 @@ from config.settings.base import env
 DEBUG = False
 
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['localhost','127.0.0.1', '.niangular.com'])
+# Database
+DATABASES['default'] = env.db('DATABASE_URL')
+DATABASES['default']['ATOMIC_REQUESTS'] = True
+DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)
